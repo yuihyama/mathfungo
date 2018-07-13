f4fcde164e5fc6ee58f2cb012ed48e9375e1832d
yuihyama <yuihyama@gmail.com>
2018-07-14 07:27:17 +0900

    update

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	// "./add"
 	// "./sub"
 	// "./mul"
+	// "./div"
 )
 
 func main() {
@@ -14,4 +15,9 @@ func main() {
 	// fmt.Println(sub.Sub(2, 1))
 	// fmt.Println(mul.Mul(2, 1))
 	// fmt.Println(mul.Mul(-2.2, -2.1))
+	// fmt.Println(div.Div(0, 0))
+	// fmt.Println(div.Div(1, 0))
+	// fmt.Println(div.Div(-1, 0))
+	// fmt.Println(div.Div(10.0, 2.0))
+	// fmt.Println(div.Div(6.4, 2.0))
 }
