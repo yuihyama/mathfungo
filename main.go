ab2141097da54962707af7a7f7146770df801a41
yuihyama <yuihyama@gmail.com>
2018-07-16 10:14:07 +0900

    clean

--- main.go
+++ main.go
@@ -2,26 +2,8 @@ package main
 
 import (
 	"fmt"
-	// "./add"
-	// "./sub"
-	// "./mul"
-	// "./div"
-	// "./square"
-	// "./cube"
 )
 
 func main() {
 	fmt.Println("hello, world")
-
-	// fmt.Println(add.Add(1, 2))
-	// fmt.Println(sub.Sub(2, 1))
-	// fmt.Println(mul.Mul(2, 1))
-	// fmt.Println(mul.Mul(-2.2, -2.1))
-	// fmt.Println(div.Div(0, 0))
-	// fmt.Println(div.Div(1, 0))
-	// fmt.Println(div.Div(-1, 0))
-	// fmt.Println(div.Div(10.0, 2.0))
-	// fmt.Println(div.Div(6.4, 2.0))
-	// fmt.Println(square.Square(-2))
-	// fmt.Println(cube.Cube(-3))
 }
